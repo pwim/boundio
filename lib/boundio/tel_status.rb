404d97c3bb264aeacce03bfdce7cef0ff27b239e
Paul McMahon <paul@mobalean.com>
2012-03-23 13:03:43 +0900

    method to tell if call still being processed

--- lib/boundio/tel_status.rb
+++ lib/boundio/tel_status.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 module Boundio
   class TelStatus < Resource
     attr_accessor :id, :from, :to, :start, :end, :duration, :status
@@ -17,6 +18,10 @@ module Boundio
       def parse(res)
         new(res.map {|k,v| [k.sub(/^_/, ""), v] })
       end
+    end
+
+    def processing?
+      status == "架電待機"
     end
 
     def to_s
--- test/test_tel_status.rb
+++ test/test_tel_status.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 require "test_helper"
 
 class TestTelStatus < BoundioTest
@@ -5,5 +6,10 @@ class TestTelStatus < BoundioTest
     stub_request(:get, "https://boundio.jp/api/vd1/user_serial_id/tel_status?auth=user_authentication_key&key=api_key&tel_id=5").to_return(:body => {:success => "true", :result => [:_id => 5]}.to_json)
     call_status = Boundio::TelStatus.find(5)
     assert_equal 5, call_status.id
+  end
+
+  def test_status
+    assert Boundio::TelStatus.new(:status => "架電待機").processing?
+    assert !Boundio::TelStatus.new(:status => "不在").processing?
   end
 end
